a7943c1b1f18ff07a5e47863d230dcd4624a2e4b
Katarina Blagojevic <katblag@gmail.com>
2013-09-10 15:46:20 -0700

    fbook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FBOOK_APP_ID'], ENV['FBOOK_APP_SECRET'],
-           :scope => 'email,username,location,publish_actions,read_stream'
+           :scope => 'email,read_stream'
 end
