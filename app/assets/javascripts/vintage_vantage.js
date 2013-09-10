da9e71343aa355f98303c4152914db6961e1e55a
Katarina Blagojevic <katblag@gmail.com>
2013-09-10 16:57:59 -0700

    backbone pushstate

--- app/assets/javascripts/vintage_vantage.js
+++ app/assets/javascripts/vintage_vantage.js
@@ -18,7 +18,7 @@ window.VV = {
     this._installCatSideBar($catEl, categories);
 
     new VV.Routers.Items($rootEl, new VV.Collections.Items());
-    Backbone.history.start();
+    Backbone.history.start({pushState: true});
   },
   _installCatSideBar: function ($catList, categories){
     var catIndexView = new VV.Views.CategoriesIndex(categories);
