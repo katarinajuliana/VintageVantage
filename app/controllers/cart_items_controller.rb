32f4e6fd734d6f759690ce5df427b1d922643129
Katarina Blagojevic <katblag@gmail.com>
2013-09-14 19:37:35 -0700

    fixed cart item controller create bug

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,12 +2,13 @@ class CartItemsController < ApplicationController
   respond_to :json
   
   def create
+    session[:cart_item_ids] ||= []
+    
     if Item.find(params[:item_id]).sold || session[:cart_item_ids].include?(params[:item_id])
       render :json => false, :status => 422
       return
     end
     
-    session[:cart_item_ids] ||= []
     session[:cart_item_ids] << params[:item_id]
     
     render :json => Item.find(params[:item_id])
